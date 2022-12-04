16b4f9ce458692008649ba8398e1beb05a22a405
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-12-04 18:48:10 +0530

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,21 @@
+from info import BOT_USERNAME, DEL_SECOND
+from plugins.malik.extra import IYGL, GRP_IT_LK, DP_YRS, YTILK, PPI, SMART_PIC
+
+
+
+
+
+class malik(object):
+      u_name = BOT_USERNAME 
+      int_link = IYGL
+      smart_pic = SMART_PIC 
+      delete = DEL_SECOND
+      grp_it_lk = GRP_IT_LK
+      dp_yrs = DP_YRS
+    #  mntf = MNTFN
+     # ntatm =  PPI
+      ytilk = YTILK
+     # mqtt = MQTT
+     # tfarde = TFRADE
+      
+
